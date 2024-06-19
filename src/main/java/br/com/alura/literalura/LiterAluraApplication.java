a87002a740ee13d53aa37bb4a620c1e006c6c736
Gabriel Figueiredo <gabrielb.defigueiredo@gmail.com>
2024-06-19 20:37:02 -0300

    concluido

---
+++ src/main/java/br/com/alura/literalura/LiterAluraApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.literalura;
+
+
+import br.com.alura.literalura.main.Principal;
+import br.com.alura.literalura.repository.AutorRepository;
+import br.com.alura.literalura.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+    @Autowired
+    LivroRepository livroRepository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiterAluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(livroRepository, autorRepository);
+        principal.menu();
+
+
+    }
+
+}
